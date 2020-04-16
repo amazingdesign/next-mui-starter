bc0952982e07ddbaa18a620f4517b691923a9fea
Mateusz <chomamateusz@gmail.com>
2020-04-16 13:55:06 +0200

    Fast images

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,60 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import React from 'react'
+
+import Img from 'react-image'
+
+import { makeStyles, Box, Container, Typography } from '@material-ui/core'
+
+import ProTip from '../src/ProTip'
+import Link from '../src/bits/Link'
+import Copyright from '../src/Copyright'
+
+const useStyles = makeStyles((theme) => ({
+  main: {},
+  img: {
+    width: '100%',
+  },
+  icon: {
+    width: 100,
+    margin: '50px auto',
+    display: 'block',
+  },
+}))
 
 export default function Index() {
+  const classes = useStyles()
+
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
+    <main
+      className={classes.main}
+    >
+      <Img
+        className={classes.img}
+        src={[
+          require('../images/astronaut.jpg?webp'),
+          require('../images/astronaut.jpg')
+        ]}
+        loader={<img src={require('../images/astronaut.jpg?trace').trace} alt={'loader'} />}
+      />
+      <Img
+        className={classes.icon}
+        src={require('../images/next-js.svg?inline')}
+      />
+      <Container maxWidth="sm">
+        <Box my={4}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Next.js example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <Link href="/next" color="secondary">
-          Go to the next.js page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
+          <Link href="/about" color="secondary">
+            Go to the about page
+          </Link>
+          <br />
+          <Link href="/next" color="secondary">
+            Go to the next.js page
+           </Link>
+          <ProTip />
+          <Copyright />
+        </Box>
+      </Container>
+    </main>
+  )
 }
